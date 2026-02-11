8bfc3f9a55de10824c0c36f6a16fa11e34454302
YueYuuta <jcjunior308@gmail.com>
2026-02-11 11:26:47 -0500

    test: setup jest and configure testing environment

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  roots: ['<rootDir>'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+  },
+};
+
+export default config;
